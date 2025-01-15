6e242d66c6d591e6753e7a4766d9285bd2d22c32
Pr1nkos <prinkos1@gmail.com>
2025-01-15 22:34:47 +0300

    1

--- gamedata/scripts/dynamic_translations/ui_st_mm.lua
+++
@@ -1,7 +0,0 @@
--- mm.lua
--- Переводы для интерфейса и игровых элементов
-
-local translations = {
-	["ui_mm_quit2windows"] = "!Выйти в Windows",
-}
-return translations
---
+++ gamedata/scripts/xml_translator/xml_translator_config.lua
@@ -0,0 +1,16 @@
+-- xml_translator_config.lua
+-- Конфигурационный файл для управления функциями
+
+return {
+    -- Включить/выключить обработку переводов
+    enable_translations = true,
+
+    -- Включить/выключить создание файлов missing_translations_rus
+    enable_missing_translations = true,
+
+    -- Включить/выключить применение переводов из dynamic_translations
+    enable_dynamic_translations = true,
+
+    -- Включить/выключить логирование
+    enable_logging = true,
+}
